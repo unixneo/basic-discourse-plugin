5d1c5755c9c142495e5ac111a4c8010b4e9ba0a7
neounix <neo@unix.com>
2020-05-10 23:30:21 +0700

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -4,7 +4,7 @@
 # authors: Awesome Plugin Developer
 after_initialize do
     Topic.register_custom_field_type(:import_id, :string)
-    add_to_serializer(:topic_view, :import_id) do
+    add_to_serializer(:topic_view, :import_id, false) do
         object.topic.custom_fields["import_id"] 
     end
 end
