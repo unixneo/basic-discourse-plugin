928963c8a46e09cae9ecc83da024890c9ce5e15d
neounix <neo@unix.com>
2020-05-10 22:37:23 +0700

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -2,3 +2,7 @@
 # about: A super simple plugin to demonstrate how plugins work
 # version: 0.0.1
 # authors: Awesome Plugin Developer
+after_initialize do
+    Topic.register_custom_field_type(:import_id, :string)
+    add_to_serializer(:topic_view, :import_id) { object.topic.import_id }
+end
