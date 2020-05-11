cddab8a1d57480f89860980d2f95a799fab0dcf3
Tim Bass <neo@unix.com>
2020-05-11 22:14:52 +0700

    FIX:  revert changes in plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,11 +1,8 @@
-# name: legacy_forum_info
+# name: basic-plugin
 # about: A super simple plugin to demonstrate how plugins work
 # version: 0.0.1
 # authors: Awesome Plugin Developer
 
-enabled_site_setting :legacy_forum_info_enabled
-
-PLUGIN_NAME ||= 'LegacyForumInfo'
 
 register_asset "stylesheets/common/basic-import-id.scss"
 
