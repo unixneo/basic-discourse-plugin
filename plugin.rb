77a071939aa4f189ad1107e2790e95235f28c69b
Tim Bass <neo@unix.com>
2020-05-11 23:10:21 +0700

    TRY: comment out console.log in init

--- plugin.rb
+++ plugin.rb
@@ -1,8 +1,8 @@
 # name: basic-plugin
 # about: A super simple plugin to demonstrate how plugins work
 # version: 0.0.1
-# authors: Awesome Plugin Developer
-
+# authors: Neo, spirobel
+# url: https://github.com/unixneo/basic-discourse-plugin
 
 register_asset "stylesheets/common/basic-import-id.scss"
 
