8adfbb41ee4ee8dce912d81d2c24f003a6adce0a
Tim Bass <neo@unix.com>
2020-05-12 09:54:51 +0700

    REVER: enable_site_setting

--- plugin.rb
+++ plugin.rb
@@ -1,12 +1,10 @@
-# frozen_string_literal: true
-
 # name: basic-plugin
 # about: A super simple plugin to demonstrate how plugins work
 # version: 0.0.1
 # authors: Neo, spirobel
 # url: https://github.com/unixneo/basic-discourse-plugin
 
-enabled_site_setting :legacy_enabled
+#enabled_site_setting :legacy_enabled
 
 register_asset "stylesheets/common/basic-import-id.scss"
 
